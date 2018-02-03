8d98b73a6daa476019a6ce730910103d846803a7
Vicente Golveo <binzu@Vicentes-Air.home>
2018-02-02 20:14:17 -0500

    add bootstrap, sass, form and http modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
-
 
 @NgModule({
   declarations: [
@@ -12,7 +12,9 @@ import { TasksComponent } from './tasks/tasks.component';
     TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
